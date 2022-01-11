175ed4b16092ecaea37367ef30c826341948b78d
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-01-10 21:06:49 -0300

    add navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <header>
+    <header class="navbar">
       <a href=""></a>
-      <ul>
+      <div class="navbar__mobile">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+      <ul class="navbar__itens active">
         <li>About Me</li>
         <li>Tech Stack</li>
         <li>Projects</li>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
       scss: {
         additionalData: `
         @import "~@/assets/scss/color/colors.scss";
-        @import "~@/assets/scss/global.scss";`
+        @import "~@/assets/scss/global.scss";
+        @import "~@/assets/scss/navbar/index.scss";`
       }
     }
   }
