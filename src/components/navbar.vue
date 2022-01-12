9c703e52d3380a9b837086365d5f4fed5573112f
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-01-12 09:16:25 -0300

    add navbar responsive

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,32 +2,55 @@
   <div>
     <header class="navbar">
       <a href=""></a>
-      <div class="navbar__mobile">
-        <div class="line1"></div>
-        <div class="line2"></div>
-        <div class="line3"></div>
+      <div class="navbar__mobile" @click="showMenu()">
+        <div class="line1" :class="{line1: menuActive}"></div>
+        <div class="line2" :class="{line2: menuActive}"></div>
+        <div class="line3" :class="{line3: menuActive}"></div>
       </div>
-      <ul class="navbar__itens active">
-        <li>About Me</li>
-        <li>Tech Stack</li>
-        <li>Projects</li>
-        <li>Resources</li>
-        <li>Contact</li>
-        <li>Button</li>
+      <ul
+        class="navbar__itens"
+        :class="{ active: menuActive, notActive: !menuActive }"
+      >
+        <li class="navbar__links">About Me</li>
+        <li class="navbar__links">Tech Stack</li>
+        <li class="navbar__links">Projects</li>
+        <li class="navbar__links">Resources</li>
+        <li class="navbar__links">Contact</li>
+        <li class="navbar__links">Button</li>
       </ul>
     </header>
   </div>
 </template>
 <script>
 export default {
-  name: 'navbar',
+  name: "navbar",
   data() {
     return {
-      
-    }
+      menuActive: false,
+    };
   },
   methods: {
-    
+    showMenu() {
+      this.menuActive = !this.menuActive;
+    },
   },
-}
+};
 </script>
+<style lang="scss">
+
+.notActive {
+  transform: translateX(100%);
+  li {
+    opacity: 0;
+  }
+}
+
+.active {
+  transform: translateX(0%);
+
+  li {
+    opacity: 1;
+    color: red;
+  }
+}
+</style>
